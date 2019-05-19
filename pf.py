ccfdb31c8a97173f3bae50d2da62ec9c51f4242e
Chris Greene <archgoon@gmail.com>
2019-05-19 13:01:05 -0700

    Add Dynamics2
    This is a verlet integration attempt.
    
    Initial results indicate that it's not as good as tracking position
    and velocity.

--- pf.py
+++ pf.py
@@ -5,7 +5,7 @@ DIMENSION = 2
 FACTORS = 2 # Position, velocity
 
 MODEL_SIGMA = 1
-ACCEL_DRIFT = 3
+ACCEL_DRIFT = 1
 # circular particle
 def signal_x(t):
     return numpy.stack((numpy.cos(t), numpy.sin(t)), 1)
@@ -21,9 +21,16 @@ def dynamics(particles, a, dt):
     x_drift = .5 * a_drift * dt ** 2 + p_v * dt + p_x
     v_drift = a * dt + p_v
 
-    assert(x_drift.shape== p_x.shape)
-
     return numpy.stack((x_drift, v_drift), 1)
+
+def dynamics2(particles, a, dt):
+    p_x = particles[:,0,:]
+    p_xp = particles[:,1,:]
+    
+    a_drift = a + numpy.random.normal(0, ACCEL_DRIFT, size=p_x.shape)
+    p_xn1 = a_drift * dt**2 + 2*p_x - p_xp
+
+    return numpy.stack((p_xn1, p_x), 1)
 
 def likelihood(particles, measurement, weights):
     p_x = particles[:,0,:]
@@ -39,7 +46,7 @@ def particle_filter(n, control, measurements, truth, dt):
     w = 1.0/n  + numpy.zeros(n)
     for u, m, t in zip(control, measurements, truth):
         # Do dynamics
-        drift_p = dynamics(p, u, dt)
+        drift_p = dynamics2(p, u, dt)
         # compute weight based on measurements and existing weights
         w = likelihood(drift_p, m, w)
         print(numpy.sum(w))
